11c6bb1bd6d3bc4616641bba8d66ed42cb4de086
Simo Soini <simo@capscience.fi>
2025-10-20 16:34:34 +0300

    part4: complete exercises 4.1-4.2

---
+++ part4/app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+app.use(express.static('dist'))
+app.use(express.json())
+
+morgan.token('post_data', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  } else {
+    return ''
+  }
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_data'))
+
+logger.info('connecting to', config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
