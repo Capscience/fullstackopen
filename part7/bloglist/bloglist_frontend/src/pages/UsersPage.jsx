d20d14cabf82c88cf95f4707eee0c371b3854aa6
Simo Soini <simo@capscience.fi>
2026-02-06 12:51:53 +0200

    part7: 14: käyttäjien näkymä

--- part7/bloglist/bloglist_frontend/src/pages/UsersPage.jsx
+++ part7/bloglist/bloglist_frontend/src/pages/UsersPage.jsx
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { initializeUsers } from "../reducers/userReducer";
+
 const UsersPage = () => {
-  return (
-    <></>
-  )
+  const dispatch = useDispatch();
+  const { user, users } = useSelector(state => state);
+  useEffect(() => {
+    dispatch(initializeUsers());
+  }, [dispatch])
+  return user ? (
+    <>
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Blogs</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr>
+              <td>{user.name}</td>
+              <td style={{ textAlign: 'center' }}>{user.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  ) : null;
 }
 
 export default UsersPage;
